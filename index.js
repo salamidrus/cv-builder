8cf16a81a92c70afc7d877f4d9a12be2a8183f2e
salamidrus <idrussalam95gmail.com>
2021-02-17 20:44:49 +0700

    Add Static Assets Checker

--- index.js
+++ index.js
@@ -30,3 +30,15 @@ app.post("/create-pdf", (req, res) => {
 app.get("/fetch-pdf", (req, res) => {
   res.sendFile(`${__dirname}/CV.pdf`);
 });
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
