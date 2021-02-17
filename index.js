7988414bebb39be0b0c2fa97b293b0695649ded9
salamidrus <idrussalam95gmail.com>
2021-02-17 20:37:51 +0700

    Add Create PDF API

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const pdf = require("html-pdf");
 const cors = require("cors");
 
 const pdfTemplate = require("./documents");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// PDF Generator
+app.post("/create-pdf", (req, res) => {
+  pdf.create(pdfTemplate(req.body), {}).toFile("Resume.pdf"),
+    (err) => {
+      if (err) {
+        res.send(Promise.reject());
+        console.log(err);
+      }
+
+      res.send(Promise.resolve());
+      console.log("Success");
+    };
+});
